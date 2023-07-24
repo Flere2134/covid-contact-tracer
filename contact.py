7d64b06d38146742011d602e4d511fe2a0fb85d3
Flere21 <aaronhans2134@gmail.com>
2023-07-24 17:06:13 +0800

    finished Contact class and added info needed for contacts

--- contact.py
+++ contact.py
@@ -1,2 +1,9 @@
 class Contact:
     #create constructor for contact info
+    def __init__(self, name, number, location, date_of_visit):
+        self.name = name
+        self.number = number
+        self.location = location
+        self.date_of_visit = date_of_visit
+    def __str__(self):
+        return f'Name: {self.name}\nPhone Number: {self.number}\nLocation: {self.location}\nDate of Visit: {self.date_of_visit}'
