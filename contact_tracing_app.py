83de93e7a6e8c212f596ce73db633a4147b6b1a6
Flere21 <aaronhans2134@gmail.com>
2023-07-24 18:21:59 +0800

    added search and add contact method in contact tracing app class

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -3,5 +3,17 @@ from file_handler import FileHandler
 
 class ContactTracingApp:
     #constructor
+    def __init__(self, filename = "contacts.txt"):
+        self.filename = filename
     #add contact method
+    def add_contact(self, name, number, location, date_of_visit):
+        contact = Contact(name, number, location, date_of_visit)
+        FileHandler.write_contact(contact, self.filename)
     #search contact method
+    def search_contact(self, search_term):
+        contacts = FileHandler.read_contact(self.filename)
+        matching_contacts = []
+        for contact in contacts:
+            if search_term.lower() in contact.name.lower() or search_term in contact.number:
+                matching_contacts.append(contact)
+        return matching_contacts
