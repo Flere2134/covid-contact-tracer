f317e14596f1be8fa1d1ce7ecbaee127c5da0d5f
Flere21 <aaronhans2134@gmail.com>
2023-07-25 14:20:09 +0800

    added entry boxes for the labels

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def add_contact_entry():
     name = name_entry.get()
     number = number_entry.get()
     location = location_entry.get()
-    date_of_visit = date_of_visit_entry.get()
+    date_of_visit = date_entry.get()
 
     app.add_contact(name, number, location, date_of_visit)
     messagebox.showinfo("Contact added")
 
 def search_contact_entry():
-    search_term = search_term_entry.get()
+    search_term = search_entry.get()
     matching_contacts = app.search_contact(search_term)
     if matching_contacts:
         result_text.delete("1.0", tk.END)
@@ -39,14 +39,26 @@ def main():
         app = ContactTracingApp("contacts.txt")
 
         root = tk.Tk()
+        root.geometry("250x250")
         root.title("COVID-19 Contact Tracing App") #title of GUI
 
         #widgets
-        tk.Label(root, text = "Name:").grid(row=0, column=0, sticky=tk.W)
-        tk.Label(root, text= "Phone Number:").grid(row=1, column=0, sticky=tk.W)
-        tk.Label(root, text= "Location visited:").grid(row=2, column=0, sticky=tk.W)
-        tk.Label(root, text= "Date visited:").grid(row=3, column=0, sticky=tk.W)
+        tk.Label(root, text = "Name:", font = ("Helveltica", 10)).grid(row=0, column=0, sticky=tk.W)
+        tk.Label(root, text= "Phone Number:", font = ("Helveltica", 10)).grid(row=1, column=0, sticky=tk.W)
+        tk.Label(root, text= "Location visited:", font = ("Helveltica", 10)).grid(row=2, column=0, sticky=tk.W)
+        tk.Label(root, text= "Date visited:", font = ("Helveltica", 10)).grid(row=3, column=0, sticky=tk.W)
 
+        global name_entry, number_entry, location_entry, date_entry, search_entry, result_text 
+
+        name_entry = tk.Entry(root)
+        number_entry = tk.Entry(root)
+        location_entry = tk.Entry(root)
+        date_entry = tk.Entry(root)
+
+        name_entry.grid(row=0, column=1)
+        number_entry.grid(row=1, column=1)
+        location_entry.grid(row=2, column=1)
+        date_entry.grid(row=3, column=1)
 
 
 
