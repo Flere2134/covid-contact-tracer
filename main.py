8cc7cce52370342a863d9f77f269c75937f7d36a
Flere21 <aaronhans2134@gmail.com>
2023-07-25 14:42:16 +0800

    created window for login in the app

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import tkinter as tk
 from tkinter import messagebox
 
 def login():
-    while True:
         username = input("Enter username: ")
         password = input("Enter password: ")
 
         if username == "User123" and password == "1234":
-            return True
+            login_window.destroy()
+            main.app()
         else:
-            print("Invalid. Please try again!")
+            messagebox.showerror("INVALID INPUT. PLEASE TRY AGAIN!")
+
+login_window = tk.Tk()
+login_window.geometry("200x150")
+login_window.title("User Login")
+    
 
 def add_contact_entry():
     name = name_entry.get()
@@ -38,40 +43,41 @@ def main():
         global app
         app = ContactTracingApp("contacts.txt")
 
-        root = tk.Tk()
-        root.geometry("280x300")
-        root.title("COVID-19 Contact Tracing App") #title of GUI
+        contact_tracing_app_window = tk.Tk()
+        contact_tracing_app_window.geometry("280x300")
+        contact_tracing_app_window.title("COVID-19 Contact Tracing App") #title of GUI
 
         #widgets
-        tk.Label(root, text = "Name:", font = ("Helveltica", 10)).grid(row=0, column=0, sticky=tk.W)
-        tk.Label(root, text= "Phone Number:", font = ("Helveltica", 10)).grid(row=1, column=0, sticky=tk.W)
-        tk.Label(root, text= "Location visited:", font = ("Helveltica", 10)).grid(row=2, column=0, sticky=tk.W)
-        tk.Label(root, text= "Date visited:", font = ("Helveltica", 10)).grid(row=3, column=0, sticky=tk.W)
+        tk.Label(contact_tracing_app_window, text = "Name:", font = ("Helveltica", 10)).grid(row=0, column=0, sticky=tk.W)
+        tk.Label(contact_tracing_app_window, text= "Phone Number:", font = ("Helveltica", 10)).grid(row=1, column=0, sticky=tk.W)
+        tk.Label(contact_tracing_app_window, text= "Location visited:", font = ("Helveltica", 10)).grid(row=2, column=0, sticky=tk.W)
+        tk.Label(contact_tracing_app_window, text= "Date visited:", font = ("Helveltica", 10)).grid(row=3, column=0, sticky=tk.W)
 
         global name_entry, number_entry, location_entry, date_entry, search_entry, result_text 
 
-        name_entry = tk.Entry(root)
-        number_entry = tk.Entry(root)
-        location_entry = tk.Entry(root)
-        date_entry = tk.Entry(root)
+        name_entry = tk.Entry(contact_tracing_app_window)
+        number_entry = tk.Entry(contact_tracing_app_window)
+        location_entry = tk.Entry(contact_tracing_app_window)
+        date_entry = tk.Entry(contact_tracing_app_window)
 
         name_entry.grid(row=0, column=1)
         number_entry.grid(row=1, column=1)
         location_entry.grid(row=2, column=1)
         date_entry.grid(row=3, column=1)
 
-        button_1 = tk.Button(root, text = "Add Contact", command = add_contact_entry)
+        button_1 = tk.Button(contact_tracing_app_window, text = "Add Contact", command = add_contact_entry)
         button_1.grid(row=4, column=0, columnspan=2, pady=5)
 
-        tk.Label(root, text = "Search Contact:", font = ("Helveltica", 10)).grid(row=5, column=0, sticky=tk.W)
-        search_entry = tk.Entry(root)
+        tk.Label(contact_tracing_app_window, text = "Search Contact:", font = ("Helveltica", 10)).grid(row=5, column=0, sticky=tk.W)
+        search_entry = tk.Entry(contact_tracing_app_window)
         search_entry.grid(row=5, column=1)
-        button_2 = tk.Button(root, text = "Search", command = search_contact_entry)
+        button_2 = tk.Button(contact_tracing_app_window, text = "Search", command = search_contact_entry)
         button_2.grid(row=6, column=0, columnspan=2, pady=5)
 
-        result_text = tk.Text(root, height=10, width=40)
+        result_text = tk.Text(contact_tracing_app_window, height=10, width=40)
         result_text.grid(row=7, column=0, columnspan=2, pady=5)
 
+        contact_tracing_app_window.mainloop()
 
 if __name__ == "__main__":
     main()
