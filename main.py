751c846f1fc36804f7cc8df1bf1868c67df454a6
Flere21 <aaronhans2134@gmail.com>
2023-07-25 14:09:46 +0800

    added labels for contact entry

--- main.py
+++ main.py
@@ -41,30 +41,14 @@ def main():
         root = tk.Tk()
         root.title("COVID-19 Contact Tracing App") #title of GUI
 
-        while True:
-            print("\nCOVID Contact Tracing App\n1. Add a Contact\n2. Search a Contact\n3. Exit")
+        #widgets
+        tk.Label(root, text = "Name:").grid(row=0, column=0, sticky=tk.W)
+        tk.Label(root, text= "Phone Number:").grid(row=1, column=0, sticky=tk.W)
+        tk.Label(root, text= "Location visited:").grid(row=2, column=0, sticky=tk.W)
+        tk.Label(root, text= "Date visited:").grid(row=3, column=0, sticky=tk.W)
 
-            choose = input("Enter your choice: ")
 
-            if choose == "1":
-                name = input("Enter Name: ")
-                number = input("Enter Phone Number: ")
-                location = input("Enter Location visited: ")
-                date_of_visit = input("Enter the date visited: ")
-                app.add_contact(name, number, location, date_of_visit)
-            elif choose == "2":
-                search_term = input("Enter Contact Name or Phone Number: ")
-                matching_contacts = app.search_contact(search_term)
-                if matching_contacts:
-                    print("\nMatching Contacts:")
-                    for contact in matching_contacts:
-                        print(contact)
-                else:
-                    print("No matched contact found")
-            elif choose == "3":
-                break
-            else:
-                print("Invalid choice. Please try again")
+
 
 if __name__ == "__main__":
     main()
