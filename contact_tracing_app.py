b7382771da259734969e9078ce6d80955e0ffb97
Flere21 <aaronhans2134@gmail.com>
2023-07-25 13:51:45 +0800

    fixed variable name casing

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -3,16 +3,16 @@ from file_handler import FileHandler
 
 class ContactTracingApp:
     #constructor
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, file_name):
+        self.file_name = file_name
         self.file_handler = FileHandler()
     #add contact method
     def add_contact(self, name, number, location, date_of_visit):
         contact = Contact(name, number, location, date_of_visit)
-        self.file_handler.write_contact(contact, self.filename)
+        self.file_handler.write_contact(contact, self.file_name)
     #search contact method
     def search_contact(self, search_term):
-        contacts = self.file_handler.read_contact(self.filename)
+        contacts = self.file_handler.read_contact(self.file_name)
         matching_contacts = []
         for contact in contacts:
             if search_term.lower() in contact.name.lower() or search_term in contact.number:
--- file_handler.py
+++ file_handler.py
@@ -2,13 +2,13 @@ from contact import Contact
 
 class FileHandler:
     #method for writing/adding contact
-    def write_contact(self, contact, filename):
-        with open(filename, "a") as file:
+    def write_contact(self, contact, file_name):
+        with open(file_name, "a") as file:
             file.write(f'{contact.name}, {contact.number}, {contact.location}, {contact.date_of_visit}\n')
     #method for reading contact
-    def read_contact(self, filename):
+    def read_contact(self, file_name):
         contacts = []
-        with open(filename, "r") as file:
+        with open(file_name, "r") as file:
             for line in file:
                 name, number, location, date_of_visit = line.strip().split(',')
                 contacts.append(Contact(name, number, location, date_of_visit))
